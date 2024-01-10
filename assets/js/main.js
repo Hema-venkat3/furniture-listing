940d4b1afd90176a4ab48cbd8fbfc00c4ce86fc0
Hema-venkat3 <hemavenkatesan3@gmail.com>
2024-01-10 19:26:25 +0530

    final

---
+++ assets/js/main.js
@@ -0,0 +1,77 @@
+import products from "../../data/products.json";
+
+
+let product_listEl = document.getElementById("product_list");
+
+products.forEach((product) =>{
+    product_listEl.appendChild(elementStyling(product));
+});
+
+function elementStyling(product){
+// Create a new element
+let liEl = document.createElement('li');
+liEl.style.backgroundColor = 'rgb(251,146,60)';
+liEl.style.margin = '20px';
+liEl.style.textAlign = 'center';
+liEl.style.borderRadius = "10px";
+liEl.style.lineHeight = "35px";
+
+const h3El = document.createElement('h3');
+h3El.style.fontWeight = '150px';
+h3El.style.fontSize = '30px';
+h3El.style.padding = "10px";
+h3El.style.color = 'rgb(59,130,246)';
+
+
+const imgEl = document.createElement('img');
+imgEl.style.width = '300px';
+imgEl.style.height = '300px';
+imgEl.style.borderRadius = "30px";
+imgEl.style.display = 'block';
+imgEl.style.margin = 'auto';
+imgEl.style.paddingTop = "35px";
+imgEl.src = product.imgUrl; // Set the actual image source URL
+imgEl.alt = product.title;    // Set alt text for accessibility
+
+h3El.textContent = product.title;
+
+const pEl = document.createElement('p');
+pEl.innerHTML = product.description + '<br>';
+
+const spanEl = document.createElement('span');
+spanEl.innerHTML = `Star Ratings: ${product.star_ratings}` + '<br>'; 
+
+const span2El = document.createElement('span');
+span2El.style.fontSize = '30px';
+span2El.style.fontWeight = '30px';
+span2El.innerHTML = `${product.Price}` + '<br>';
+
+const smallEl = document.createElement('small');
+// smallEl.style.color = 'rgb(251,146,60)';
+smallEl.style.color = 'black';
+smallEl.style.fontWeight = '80px'
+smallEl.innerHTML = product.delivery + '<br>';
+
+const buttonEl = document.createElement('button');
+
+liEl.appendChild(imgEl);
+liEl.appendChild(h3El);
+liEl.appendChild(pEl);
+liEl.appendChild(spanEl);
+liEl.appendChild(span2El);
+liEl.appendChild(smallEl);
+liEl.appendChild(buttonEl);
+
+return liEl;
+}
+
+console.log(products);
+
+
+
+
+
+
+
+
+
