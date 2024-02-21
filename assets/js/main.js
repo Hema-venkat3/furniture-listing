a3ded542e7dc63946a90ef78d77d0c7eb0604ac0
Hema-venkat3 <hemavenkatesan3@gmail.com>
2024-02-21 21:03:20 +0530

    fixed colors

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,17 +10,17 @@ products.forEach((product) =>{
 function elementStyling(product){
 // Create a new element
 let liEl = document.createElement('li');
-liEl.style.backgroundColor = 'rgb(251,146,60)';
+liEl.style.backgroundColor = '#FBEAEB';
 liEl.style.margin = '20px';
 liEl.style.textAlign = 'center';
-liEl.style.borderRadius = "10px";
+liEl.style.borderRadius = "5px";
 liEl.style.lineHeight = "35px";
 
 const h3El = document.createElement('h3');
-h3El.style.fontWeight = '150px';
+h3El.style.fontWeight = '600';
 h3El.style.fontSize = '30px';
 h3El.style.padding = "10px";
-h3El.style.color = 'rgb(59,130,246)';
+h3El.style.color = '#1995AD';
 
 
 const imgEl = document.createElement('img');
@@ -32,6 +32,15 @@ imgEl.style.margin = 'auto';
 imgEl.style.paddingTop = "35px";
 imgEl.src = product.imgUrl; // Set the actual image source URL
 imgEl.alt = product.title;    // Set alt text for accessibility
+imgEl.style.transition = 'transform 0.3s ease';
+
+imgEl.addEventListener('mouseover', function() {
+    this.style.transform = 'scale(1.1)';
+});
+
+imgEl.addEventListener('mouseout', function() {
+    this.style.transform = 'scale(1)';
+});
 
 h3El.textContent = product.title;
 
@@ -40,19 +49,36 @@ pEl.innerHTML = product.description + '<br>';
 
 const spanEl = document.createElement('span');
 spanEl.innerHTML = `Star Ratings: ${product.star_ratings}` + '<br>'; 
+// spanEl.style.color = '#1995AD';
 
 const span2El = document.createElement('span');
-span2El.style.fontSize = '30px';
-span2El.style.fontWeight = '30px';
-span2El.innerHTML = `${product.Price}` + '<br>';
+span2El.style.fontSize = '20px';
+span2El.style.fontWeight = '500';
+span2El.style.color = '#1995AD';
+span2El.innerHTML = `Price : ${product.Price}` + '<br>';
 
 const smallEl = document.createElement('small');
 // smallEl.style.color = 'rgb(251,146,60)';
-smallEl.style.color = 'black';
-smallEl.style.fontWeight = '80px'
+smallEl.style.color = 'gray';
+// smallEl.style.fontWeight = '80px'
 smallEl.innerHTML = product.delivery + '<br>';
 
 const buttonEl = document.createElement('button');
+buttonEl.style.color = 'white';
+buttonEl.style.borderRadius = '4px';
+buttonEl.style.width = '100px';
+buttonEl.style.padding = '5px';
+buttonEl.style.background = '#1995AD';
+buttonEl.style.margin = '10px';
+buttonEl.innerHTML = product.button;
+
+buttonEl.addEventListener("mouseover", function() {
+    this.style.transform = "scale(1.1)";
+});
+
+buttonEl.addEventListener("mouseout", function() {
+    this.style.transform = "scale(1)";
+});
 
 liEl.appendChild(imgEl);
 liEl.appendChild(h3El);
